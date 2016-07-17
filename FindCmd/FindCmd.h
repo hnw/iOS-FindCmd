bab1214e3c399496297502ff6d5ea0cb3fa44f28
Yoshio HANAWA <y@hnw.jp>
2016-07-17 14:35:06 +0900

    Initial commit

---
+++ FindCmd/FindCmd.h
@@ -0,0 +1,19 @@
+//
+//  FindCmd.h
+//  FindCmd
+//
+//  Created by hnw on 2016/07/17.
+//  Copyright © 2016年 hnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FindCmd.
+FOUNDATION_EXPORT double FindCmdVersionNumber;
+
+//! Project version string for FindCmd.
+FOUNDATION_EXPORT const unsigned char FindCmdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FindCmd/PublicHeader.h>
+
+
